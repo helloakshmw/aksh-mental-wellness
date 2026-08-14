3d198b5ed903f8644b6b374a8ee996e0bf75df55
helloakshmw <hello.akshmw@gmail.com>
2026-08-14 11:48:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const enterButton = document.getElementById("enterButton");
 
+enterButton.addEventListener("click", () => {
+  document.body.classList.add("entering");
+
+  setTimeout(() => {
+    alert("AKSH is opening. The journey begins here.");
+  }, 350);
+});
